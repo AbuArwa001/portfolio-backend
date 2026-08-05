4e25037985a8817342722e3d4c13b878fae4f205
AbuArwa001 <khalfanathman12@yahoo.com>
2026-08-05 14:15:12 +0300

    feat: add credential tracking fields to Certification model and update serializer fields

--- certifications/serializers.py
+++ certifications/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Certification
 
+
 class CertificationSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.id')
+
     class Meta:
         model = Certification
-        fields = "__all__"
+        fields = [
+            'id', 'user', 'name', 'issuer', 'date',
+            'badge', 'credential_url', 'in_progress', 'type',
+        ]
