b2a8866d22ea74778d9369d528471a44b8413498
AbuArwa001 <khalfanathman12@yahoo.com>
2026-08-07 09:14:02 +0300

    feat: configure ProjectAdmin interface and update ProjectSerializer with absolute image URLs

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'status', 'completion', 'user', 'created_at')
+    list_filter = ('type', 'status', 'user')
+    search_fields = ('name', 'description', 'technologies')
+    readonly_fields = ('created_at',)
+    ordering = ('-created_at',)
--- projects/serializers.py
+++ projects/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 from .models import Project
 
+
 class ProjectSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.id')
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Project
-        fields = '__all__'
-        read_only_fields = ('user', 'created_at')
+        fields = [
+            'id', 'user', 'name', 'description', 'link',
+            'status', 'completion', 'technologies', 'type',
+            'image', 'image_url', 'created_at',
+        ]
+        read_only_fields = ('user', 'created_at', 'image_url')
+
+    def get_image_url(self, obj):
+        """Return an absolute URL for the image, or None."""
+        if not obj.image:
+            return None
+        request = self.context.get('request')
+        if request:
+            return request.build_absolute_uri(obj.image.url)
+        return obj.image.url
